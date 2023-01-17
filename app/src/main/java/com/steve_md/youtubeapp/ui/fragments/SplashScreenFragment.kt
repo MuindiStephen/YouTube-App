6081b5d72b1605b4ecf993190287b570ae8bbb9f
MuindiStephen <stephenmuindi241@gmail.com>
2023-01-17 18:32:55 +0300

    finished working on splash screen

--- app/src/main/java/com/steve_md/youtubeapp/ui/fragments/SplashScreenFragment.kt
+++ app/src/main/java/com/steve_md/youtubeapp/ui/fragments/SplashScreenFragment.kt
@@ -1,22 +1,38 @@
 package com.steve_md.youtubeapp.ui.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.steve_md.youtubeapp.R
+import com.steve_md.youtubeapp.databinding.FragmentSplashScreenBinding
 
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenFragment : Fragment() {
 
+    private lateinit var binding: FragmentSplashScreenBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+        binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(R.id.action_splashScreenFragment_to_homeFragment)
+        }, 3000)
     }
 
 
