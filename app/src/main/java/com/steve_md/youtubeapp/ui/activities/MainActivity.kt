99bb0c2966ee8ea442d41e396aa12304770a7755
MuindiStephen <stephenmuindi241@gmail.com>
2023-01-17 18:19:59 +0300

    More coding

--- app/src/main/java/com/steve_md/youtubeapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/steve_md/youtubeapp/ui/activities/MainActivity.kt
@@ -2,11 +2,28 @@ package com.steve_md.youtubeapp.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.steve_md.youtubeapp.R
+import com.steve_md.youtubeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+
+        navController = navHostFragment.navController
     }
 }
