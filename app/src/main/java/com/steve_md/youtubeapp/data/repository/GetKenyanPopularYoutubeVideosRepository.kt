f08a3585c8600b69caee874d069fe3096348c037
MuindiStephen <stephenmuindi241@gmail.com>
2023-01-19 17:21:31 +0300

    new feature: Viewmodel added

--- app/src/main/java/com/steve_md/youtubeapp/data/repository/GetKenyanPopularYoutubeVideosRepository.kt
+++ app/src/main/java/com/steve_md/youtubeapp/data/repository/GetKenyanPopularYoutubeVideosRepository.kt
@@ -1,4 +1,17 @@
 package com.steve_md.youtubeapp.data.repository
 
-class GetKenyanPopularYoutubeVideos {
+import com.steve_md.youtubeapp.data.dto.YoutubeResponse
+import com.steve_md.youtubeapp.data.network.api.ApiService
+import retrofit2.Response
+import javax.inject.Inject
+
+/**
+ * Repository to interact with the API
+ * */
+class GetKenyanPopularYoutubeVideosRepository @Inject constructor(
+    private val apiService: ApiService
+) {
+    suspend fun getKenyanPopularYoutubeVideos() : Response<YoutubeResponse> {
+        return apiService.getYoutubeVideos()
+    }
 }
