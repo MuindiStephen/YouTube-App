dcbab895ca0130ac96eaf0ae5bbac59f6cf730ec
MuindiStephen <stephenmuindi241@gmail.com>
2023-01-20 11:27:47 +0300

    minor fixes and ui polish

--- app/src/main/java/com/steve_md/youtubeapp/data/network/api/ApiService.kt
+++ app/src/main/java/com/steve_md/youtubeapp/data/network/api/ApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiService {
-    @GET("/videos")
+    @GET("videos")
     suspend fun getYoutubeVideos(
         @Query("key") key: String = "AIzaSyAv_oO_G5E4rPu8WpSapzVR3sqHESPwdzg",
         @Query("regionCode") regionCode: String = "KE",
