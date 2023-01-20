c9ca3debd0b11c2e124a7d58f75b1422eaddd4dc
MuindiStephen <stephenmuindi241@gmail.com>
2023-01-20 12:48:48 +0300

    feature:recyclerview- adapter

--- app/src/main/java/com/steve_md/youtubeapp/adapter/YoutubeVideoAdapter.kt
+++ app/src/main/java/com/steve_md/youtubeapp/adapter/YoutubeVideoAdapter.kt
@@ -1,13 +1,20 @@
 package com.steve_md.youtubeapp.adapter
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.api.load
 import com.steve_md.youtubeapp.data.dto.Item
 import com.steve_md.youtubeapp.databinding.VideoRowBinding
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 
 class YoutubeVideoAdapter : ListAdapter<Item, YoutubeVideoAdapter.YoutubeVideoViewHolder>(YoutubeVideoDiffUtil) {
     object YoutubeVideoDiffUtil : DiffUtil.ItemCallback<Item>() {
@@ -23,12 +30,16 @@ class YoutubeVideoAdapter : ListAdapter<Item, YoutubeVideoAdapter.YoutubeVideoVi
 
     inner class YoutubeVideoViewHolder(private val binding: VideoRowBinding) :
         RecyclerView.ViewHolder(binding.root) {
+
+        @RequiresApi(Build.VERSION_CODES.O)
         fun bind(youtubeVideo: Item?) {
             binding.textViewVideoTitle.text = youtubeVideo?.snippet?.title
             binding.videoImageView.load(youtubeVideo?.snippet?.thumbnails?.medium?.url)
             binding.imageViewYoutubeChannel.load(youtubeVideo?.snippet?.thumbnails?.medium?.url)
             binding.textViewYoutubeChannelName.text = youtubeVideo?.snippet?.channelTitle
             binding.textViewVideoViews.text = viewCount(youtubeVideo?.statistics?.viewCount?.toInt())
+            binding.textViewVideoUploadTime.text = formatTime(youtubeVideo?.snippet?.publishedAt)
+
 
            /* binding.textViewVideoUploadTime.text = youtubeVideo?.snippet?.publishedAt?.let {
                 upLoadedAt(
@@ -39,9 +50,53 @@ class YoutubeVideoAdapter : ListAdapter<Item, YoutubeVideoAdapter.YoutubeVideoVi
         }
     }
 
-       /*private fun upLoadedAt(publishedAt: String): String {
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun formatTime(publishedAt: String?): String {
+        val uploadDateAndTimeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
+        val uploadedAt = LocalDateTime.parse(publishedAt, uploadDateAndTimeFormat)
+        val recentDates = LocalDateTime.now().withNano(0)
+        val differenceInSeconds = ChronoUnit.SECONDS.between(uploadedAt, recentDates)
+        val differenceInDays = ChronoUnit.DAYS.between(uploadedAt, recentDates)
+        val differenceInMonths = ChronoUnit.MONTHS.between(uploadedAt, recentDates)
+        return findDifference(differenceInSeconds, differenceInDays, differenceInMonths)
+    }
 
-        }*/
+    private fun findDifference(
+        differenceInSeconds: Long,
+        differenceInDays: Long,
+        differenceInMonths: Long
+    ): String {
+        val hours = differenceInSeconds/3600
+        when (differenceInDays) {
+            in 25..31 ->{
+                return "4 weeks"
+            }
+            in 21..24 -> {
+                return "3 weeks"
+            }
+            in 14..20 -> {
+                return "2 weeks"
+            }
+            in 8..13 -> {
+                return "a week ago"
+            }
+            in 2..7 -> {
+                return "$differenceInDays days ago"
+            }
+            in 0..1 -> {
+                return "$hours hours ago"
+            }
+        }
+
+        return if (differenceInMonths in 0..1) {
+            "$differenceInMonths month ago"
+        } else {
+            "$differenceInMonths months ago"
+        }
+
+    }
+
+
 
     private fun viewCount(countViews: Int?): String {
         return when {
@@ -72,6 +127,7 @@ class YoutubeVideoAdapter : ListAdapter<Item, YoutubeVideoAdapter.YoutubeVideoVi
             ))
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: YoutubeVideoViewHolder, position: Int) {
         val youtubeVideo = getItem(position)
         holder.bind(youtubeVideo)
