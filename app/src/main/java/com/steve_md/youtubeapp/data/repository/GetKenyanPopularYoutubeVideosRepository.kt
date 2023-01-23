a463f3833bc357450b50b22b8280bdfd5578c158
MuindiStephen <stephenmuindi241@gmail.com>
2023-01-23 15:01:10 +0300

    Set-up Repository for Paging 3

--- app/src/main/java/com/steve_md/youtubeapp/data/repository/GetKenyanPopularYoutubeVideosRepository.kt
+++ app/src/main/java/com/steve_md/youtubeapp/data/repository/GetKenyanPopularYoutubeVideosRepository.kt
@@ -1,5 +1,8 @@
 package com.steve_md.youtubeapp.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.steve_md.youtubeapp.data.dto.YoutubeResponse
 import com.steve_md.youtubeapp.data.network.api.ApiService
 import retrofit2.Response
@@ -11,7 +14,17 @@ import javax.inject.Inject
 class GetKenyanPopularYoutubeVideosRepository @Inject constructor(
     private val apiService: ApiService
 ) {
+    fun getVideos() = Pager(
+        config = PagingConfig(
+            pageSize = 30,
+            maxSize = 150,
+            enablePlaceholders = false
+        ),
+        pagingSourceFactory = {YoutubeKenyanPopularVideosPagingSource(apiService)}
+    )
+
+    /*
     suspend fun getKenyanPopularYoutubeVideos() : Response<YoutubeResponse> {
         return apiService.getYoutubeVideos()
-    }
+    }*/
 }
